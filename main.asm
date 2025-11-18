dea14c770e57bde4bb8de17fc0c5bb6603c2a7b1
william <william.eonet@gmail.com>
2025-11-18 23:12:15 +0100

    initial commit

---
+++ main.asm
@@ -0,0 +1,65 @@
+ORG #8000
+
+start:
+include "initGraphics.asm"
+
+gameloop:
+  CALL #BD19 ;wait VBL
+  CALL #BB1B
+  CP 243
+  CALL Z, move_x_right
+  CALL drawSprite
+JP gameloop
+
+RET
+
+drawSprite:
+  LD B,8
+  LD HL,#C000
+  LD IX,MarioIdle
+  CALL drawBloc
+
+  LD HL,#C050 ;bloc de base #C000 + 80octet
+  LD B,8
+  CALL drawBloc
+  
+RET
+
+drawBloc:
+    PUSH BC          ;on sauvegarde le registre BC pour garder notre compteur sur le registre B (car B est modifier dans des sous-routine)
+    LD D,H           ;on sauvegarde HL dans DL, on ne peut pas utiliser PUSH car PUSH sauvegarde dans SP et on ecrase BC
+    LD E,L
+    CALL drawLine
+    LD BC,#0800     ;#0800 permet de sauter a la ligne suivante
+    LD H,D          ;on restaure HL
+    LD L,E
+    ADD HL,BC       ;On rajoute #0800 dans HL pour aller a la ligne suivante
+    POP BC          ;On restaure BC pour remettre notre compteur dans B
+    DJNZ drawBloc
+    RET
+
+drawLine:
+  LD B,6
+  drawLineLoop:
+    CALL drawPixel
+    DJNZ drawLineLoop
+  RET
+
+drawPixel:
+  LD A,(IX+0)        ;On charge l'octet en cours du sprite dans le registre A
+  LD (HL),A          ;On l'affiche
+  INC HL             ;On passe a l'octet suivant en memoire
+  INC IX             ;On passe a l'octet suivant dans le sprite
+  RET
+
+include "sprite.asm"
+spriteX: DB 0
+
+move_x_right:
+  LD A,(spriteX)
+  INC A
+  LD (spriteX),A
+RET
+
+end:
+SAVE "player.bin", #8000, end - start, AMSDOS
